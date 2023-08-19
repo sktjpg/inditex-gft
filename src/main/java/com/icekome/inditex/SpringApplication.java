703167d75233b6bfb1fd57a42e8823e2adc74bc2
Javier Pontón <n374512@gruposantander.com>
2023-08-19 12:21:51 +0200

    Change name

--- src/main/java/com/icekome/inditex/SpringApplication.java
+++ src/main/java/com/icekome/inditex/SpringApplication.java
@@ -1,15 +1,14 @@
 package com.icekome.inditex;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan
-public class InditexApplication {
+public class SpringApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(InditexApplication.class, args);
+    org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
   }
 
 }
